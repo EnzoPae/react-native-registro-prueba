92546ab7c55645b2bebbca57b6d33bcbdcec491c
Federico Acosta <fede-2800@hotmail.com>
2022-12-15 15:15:14 -0300

    Se agrego boton centrado y peticion a la API

--- screens/registro.js
+++ screens/registro.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text, TextInput, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
 import { Input, Button } from "@rneui/themed";
-
+import {getUsers} from '../api/userAPI';
 const Registro = ({ navigation }) => {
+  const handlePress = async() =>{
+    try {
+      const response = await getUsers()
+      console.log(response.data)
+      if(response.status === 200){
+        console.log('Salio todo bien')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={{justifyContent: 'center',
+        alignItems: 'center'}}
+        onPress={handlePress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
       <Input placeholder="User" />
       <Input placeholder="Password" secureTextEntry={true} />
       <Input placeholder="Confirm Password" secureTextEntry={true} />
@@ -23,9 +41,15 @@ const Registro = ({ navigation }) => {
         containerStyle={{
           width: 250,
         }}
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
         title="Olvidaste tu Contraseña"
         type="clear"
-        titleStyle={{ color: "rgba(78, 116, 289, 1)" }}
+        titleStyle={{
+          color: "rgba(78, 116, 289, 1)"
+        }}
       />
     </SafeAreaView>
   );
