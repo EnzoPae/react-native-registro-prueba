dbf613a0a0664220d4c22ea346422cb088ed68d2
Federico Acosta <fede-2800@hotmail.com>
2022-12-16 15:38:00 -0300

    Se agtrego peticion a la API para crear usuario

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -3,3 +3,6 @@ import axios from 'axios'
 export const getUsers = async()=>{
     return axios.get('https://jsonplaceholder.typicode.com/users')
 }
+export const createUser = async(userData)=>{
+    return axios.post('http://192.168.0.27:4005/api/user',userData)
+}
