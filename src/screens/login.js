c1094701eef9a33460e1d96e7a205ddcf3d63d36
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2022-12-24 19:58:01 -0300

    Se creo la pantalla de login (aun falta aplicar la logica)

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,12 +1,122 @@
-import React from "react"
-import { View } from "react-native";
+import React from "react";
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
-const Registro2 = () => {
-  return(
-    <View></View>
-  )
-}
+//External libraries
+import { Input } from "@rneui/themed";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
+//Components
+import MyButton from "../components/MyButton";
 
+//Styles
+import { Colors } from "../styles/Colors";
+import { globalStyles } from "../styles/GlobalStyles";
 
-export default Registro2;
+//Start
+
+const Login = () => {
+  return (
+    <SafeAreaView style={globalStyles.loginScreenContainer}>
+      <StatusBar />
+      <ScrollView
+        contentContainerStyle={{ alignItems: "center" }}
+        style={styles.scroll}
+      >
+        <View style={globalStyles.loginScreenBlueContainer}>
+          <Text style={globalStyles.loginTitleStyle}>Bienvenido</Text>
+          <Text style={globalStyles.loginSubTitleStyle}>
+            Ingrese los datos de su cuenta para acceder
+          </Text>
+        </View>
+        <View style={globalStyles.loginScreenWhiteContainerShort}>
+          <View style={styles.inputBox}>
+            <Input
+              placeholder="Email"
+              leftIcon={<Icon name="email" size={20} />}
+              leftIconContainerStyle={globalStyles.loginInputIconStyle}
+              inputStyle={globalStyles.loginInputStyle}
+            />
+            <Input
+              placeholder="Contraseña"
+              secureTextEntry={true}
+              leftIcon={<Icon name="lock" size={20} />}
+              leftIconContainerStyle={globalStyles.loginInputIconStyle}
+              inputStyle={globalStyles.loginInputStyle}
+            />
+            <TouchableOpacity
+              onPress={() => alert("Go to recover password")}
+              style={{
+                alignItems: "flex-end",
+                marginRight: 10,
+                marginBottom: 20,
+              }}
+            >
+              <Text>¿Olvidate tu contraseña?</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              alignItems: "center",
+              marginBottom: 20,
+              marginTop: 0,
+            }}
+          >
+            <MyButton label={"INGRESAR"} size={"medium"} />
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <Text
+              style={{
+                fontSize: 12,
+                fontWeight: "bold",
+                color: Colors.grey,
+              }}
+            >
+              ¿Aun no tienes una cuenta?{" "}
+            </Text>
+            <TouchableOpacity
+              onPress={() => alert("Go to Register")}
+              style={{ flexDirection: "row" }}
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontWeight: "bold",
+                  color: Colors.primary,
+                }}
+              >
+                Regístrate
+              </Text>
+              <Icon
+                name="chevron-right-circle"
+                size={18}
+                color={Colors.primary}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Text />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    marginLeft: 25,
+    marginRight: 25,
+    marginBottom: 10,
+  },
+  scroll: {
+    width: "100%",
+  },
+});
+
+export default Login;
