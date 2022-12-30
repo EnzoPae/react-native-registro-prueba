2676b2e0af1582baaa26010c4a569778372a6fe9
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2022-12-30 01:41:57 -0300

    Se agrego la nueva pantalla para creacion de viajes (Vacia)

---
+++ src/screens/createTrip.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, ScrollView, Text, View, StatusBar } from "react-native";
+
+
+const CreateTrip = () => {
+  return (
+    <Text>Pantalla de creacion de viaje</Text>
+  );
+};
+
+export default CreateTrip;
+
+
