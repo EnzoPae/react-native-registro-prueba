ce840b7319f3633d43e4532954769a6144581e2c
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2022-12-30 01:31:03 -0300

    Se agrego la validacion mediante yup del numero de telefono

--- src/Schemas/signupFormSchema.js
+++ src/Schemas/signupFormSchema.js
@@ -1,28 +1,28 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 // SignUp.js
 export const signUpValidationSchema = yup.object().shape({
-    name: yup
-      .string()
-      .matches(/(\w.+\s).+/, 'Enter at least 2 names')
-      .required('El nombre completo es obligatorio')
-      .typeError('Pone letras'),
-    /*phoneNumber: yup
-      .string()
-      .matches(/(01)(\d){8}\b/, 'Enter a valid phone number')
-      .required('Phone number is required'),*/
-    email: yup
-      .string()
-      .email("Ingrese un mail valido")
-      .required('El email es obligatorio')
-      .typeError('Pone letras'),
-    password: yup
-      .string()
-      .min(4, ({ min }) => `La contraseña dee tener al menos ${min} caracteres`)
-      .required('La contraseña es obligatoria')
-      .typeError('Pone letras'),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password')], 'Las contraseñas no coinciden')
-      .required('Debe confirmar su contraseña')
-      .typeError('Pone letras'),
-  })
+  name: yup
+    .string()
+    .matches(/(\w.+\s).+/, "Enter at least 2 names")
+    .required("El nombre completo es obligatorio")
+    .typeError("Pone letras"),
+  phoneNumber: yup
+    .string()
+    .matches(/(01)(\d){8}\b/, "Enter a valid phone number")
+    .required("El numero de telefono es obligatorio"),
+  email: yup
+    .string()
+    .email("Ingrese un mail valido")
+    .required("El email es obligatorio")
+    .typeError("Pone letras"),
+  password: yup
+    .string()
+    .min(4, ({ min }) => `La contraseña debe tener al menos ${min} caracteres`)
+    .required("La contraseña es obligatoria")
+    .typeError("Pone letras"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Las contraseñas no coinciden")
+    .required("Debe confirmar su contraseña")
+    .typeError("Pone letras"),
+});
