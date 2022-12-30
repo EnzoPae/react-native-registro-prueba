0553dbe7ea1db0a5b349d9062b71cf958e5200fa
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2022-12-30 01:24:37 -0300

    Se agrego carga de la tipografia

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React from "react";
-import {useFonts} from 'expo-font';
-import { useEffect } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 //Screens
 import Registro from "./src/screens/registro";
 import Login from "./src/screens/login";
+import Principal from "./src/screens/principal";
+import CreateTrip from "./src/screens/createTrip";
 
 export default function App() {
-    
+  const [fontsLoaded] = useFonts({
+    'nunito': require("./assets/fonts/Nunito-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
-    <Login/>
+    <Principal/>
   );
 }
