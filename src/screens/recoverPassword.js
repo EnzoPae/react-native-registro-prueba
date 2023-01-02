17dff7785dd07c55cb3ce8494271e6f86db7e845
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-02 09:59:04 -0300

    Se agrego pantalla de recuperacion de contraseña

---
+++ src/screens/recoverPassword.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const RecoverPassword = () => {
+  return <Text>Pantalla de recuperacion de contraseña</Text>;
+};
+
+export default RecoverPassword;
