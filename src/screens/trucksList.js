ecc8fc2371a9cc2e10abbfb1e5125fa9e443ad21
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-02 10:00:31 -0300

    Se agrego pantalla de Listado de camiones

---
+++ src/screens/trucksList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const TrucksList = () => {
+  return <Text>Pantalla de lista de camiones</Text>;
+};
+
+export default TrucksList;
