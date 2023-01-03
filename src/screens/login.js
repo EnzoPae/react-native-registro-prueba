68e1fcf354aa201ac42e227579ec6f62f147c76b
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-03 03:34:22 -0300

    Se agrego formik y validaciones de campos

--- src/screens/login.js
+++ src/screens/login.js
@@ -8,105 +8,154 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+//Formik
+import { Formik } from "formik";
+import { signInValidationSchema } from "../Schemas/signinFormSchema";
+
 //Components
 import { Input } from "@rneui/themed";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import MyButton from "../components/MyButton";
 
 //Styles
 import { Colors } from "../styles/Colors";
 import { globalStyles } from "../styles/GlobalStyles";
 
-//Navigation 
+//Navigation
 import { useNavigation } from "@react-navigation/native";
 
-
 //Start
-
 const Login = () => {
-  const nav = useNavigation()
+  const nav = useNavigation();
+
+  const initialValues = {
+    email: null,
+    password: null,
+  };
 
   return (
     <SafeAreaView style={globalStyles.loginScreenContainer}>
-      <ScrollView
-        contentContainerStyle={{ alignItems: "center" }}
-        style={globalStyles.scroll}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={signInValidationSchema}
+        //onSubmit={(values) => handleEnviar(values)}
       >
-        <View style={globalStyles.loginScreenBlueContainer}>
-          <Text style={globalStyles.loginTitleStyle}>Bienvenido</Text>
-          <Text style={globalStyles.loginSubTitleStyle}>
-            Ingrese los datos de su cuenta para acceder
-          </Text>
-        </View>
-        <View style={globalStyles.loginScreenWhiteContainerShort}>
-          <View style={styles.inputBox}>
-            <Input
-              placeholder="Email"
-              leftIcon={<Icon name="email" size={20} />}
-              leftIconContainerStyle={globalStyles.loginInputIconStyle}
-              inputStyle={globalStyles.loginInputStyle}
-            />
-            <Input
-              placeholder="Contraseña"
-              secureTextEntry={true}
-              leftIcon={<Icon name="lock" size={20} />}
-              leftIconContainerStyle={globalStyles.loginInputIconStyle}
-              inputStyle={globalStyles.loginInputStyle}
-            />
-            <TouchableOpacity
-              onPress={() => nav.navigate('RecoverPassword')}
-              style={{
-                alignItems: "flex-end",
-                marginRight: 10,
-                marginBottom: 20,
-              }}
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+          isValid,
+        }) => (
+          <>
+            <ScrollView
+              contentContainerStyle={{ alignItems: "center" }}
+              style={globalStyles.scroll}
             >
-              <Text style={{fontFamily: 'nunito'}}>¿Olvidaste tu contraseña?</Text>
-            </TouchableOpacity>
-          </View>
-          <View
-            style={{
-              alignItems: "center",
-              marginBottom: 20,
-              marginTop: 0,
-            }}
-          >
-            <MyButton label={"INGRESAR"} size={"medium"} />
-          </View>
-          <View style={{ flexDirection: "row", justifyContent: "center" }}>
-            <Text
-              style={{
-                fontSize: 12,
-                fontFamily: 'nunito',
-                color: Colors.grey,
-              }}
-            >
-              ¿Aun no tienes una cuenta?{" "}
-            </Text>
-            <TouchableOpacity
-              onPress={() => nav.navigate('Registro')}
-              style={{ flexDirection: "row" }}
-            >
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontFamily: 'nunito',
-                  color: Colors.primary,
-                }}
-              >
-                Regístrate
-              </Text>
-              <Icon
-                name="chevron-right-circle"
-                size={18}
-                color={Colors.primary}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <Text />
-      </ScrollView>
+              <View style={globalStyles.loginScreenBlueContainer}>
+                <Text style={globalStyles.loginTitleStyle}>Bienvenido</Text>
+                <Text style={globalStyles.loginSubTitleStyle}>
+                  Ingrese los datos de su cuenta para acceder
+                </Text>
+              </View>
+              <View style={globalStyles.loginScreenWhiteContainerShort}>
+                <View style={styles.inputBox}>
+                  <Input
+                    name="email"
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                    placeholder="Email"
+                    leftIcon={<Icon name="email" size={20} />}
+                    leftIconContainerStyle={globalStyles.loginInputIconStyle}
+                    inputStyle={globalStyles.loginInputStyle}
+                    keyboardType="email-address"
+                    errorMessage={errors.email && touched.email && errors.email}
+                    errorStyle={globalStyles.loginInputErrorStyle}
+                  />
+                  <Input
+                    name="password"
+                    onChangeText={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    value={values.password}
+                    placeholder="Contraseña"
+                    secureTextEntry={true}
+                    leftIcon={<Icon name="lock" size={20} />}
+                    leftIconContainerStyle={globalStyles.loginInputIconStyle}
+                    inputStyle={globalStyles.loginInputStyle}
+                    errorMessage={
+                      errors.password && touched.password && errors.password
+                    }
+                    errorStyle={globalStyles.loginInputErrorStyle}
+                  />
+                  <TouchableOpacity
+                    onPress={() => nav.navigate("RecoverPassword")}
+                    style={{
+                      alignItems: "flex-end",
+                      marginRight: 10,
+                      marginBottom: 8,
+                      marginTop: 10,
+                    }}
+                  >
+                    <Text style={{ fontFamily: "nunito" }}>
+                      ¿Olvidaste tu contraseña?
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    alignItems: "center",
+                    marginBottom: 20,
+                    marginTop: 0,
+                  }}
+                >
+                  <MyButton
+                    label={"INGRESAR"}
+                    size={"medium"}
+                    onPress={handleSubmit}
+                  />
+                </View>
+                <View
+                  style={{ flexDirection: "row", justifyContent: "center" }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "nunito",
+                      color: Colors.grey,
+                    }}
+                  >
+                    ¿Aun no tienes una cuenta?{" "}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() => nav.navigate("Registro")}
+                    style={{ flexDirection: "row" }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontFamily: "nunito",
+                        color: Colors.primary,
+                      }}
+                    >
+                      Regístrate
+                    </Text>
+                    <Icon
+                      name="chevron-right-circle"
+                      size={18}
+                      color={Colors.primary}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <Text />
+            </ScrollView>
+          </>
+        )}
+      </Formik>
       <StatusBar style="light" />
     </SafeAreaView>
   );
