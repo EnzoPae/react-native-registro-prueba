26af39b53b5d8e24c3854c987396d7f8c1317b49
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-03 03:32:44 -0300

    delete

--- src/components/Fonts.js
+++
@@ -1,7 +0,0 @@
-import {useFonts} from 'expo-font';
-
-export default function MyFonts() {
-  const [fontsLoaded] = useFonts({
-    roboto: require('../../assets/fonts/RobotoSlab-VariableFont_wght.ttf'),
-  }) 
-}
