f066e4fb4302dd2f5080d77d4ad2e4f2b8156fcb
Federico Acosta <fede-2800@hotmail.com>
2023-01-09 10:53:31 -0300

    Se implemento nueva libreria para almacenamiento de token

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -4,5 +4,5 @@ export const getUsers = async()=>{
     return axios.get('https://jsonplaceholder.typicode.com/users')
 }
 export const createUser = async(userData)=>{
-    return axios.post('http://192.168.0.27:4005/api/user',userData)
+    return axios.post('http://192.168.0.169:4005/api/user',userData)
 }
