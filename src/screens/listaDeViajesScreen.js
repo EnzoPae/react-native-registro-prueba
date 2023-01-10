ffe45dda29a8dd569100a2ad0e038478248e2ae5
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-10 02:31:17 -0300

    Ajustes de diseño

--- src/screens/listaDeViajesScreen.js
+++ src/screens/listaDeViajesScreen.js
@@ -9,205 +9,180 @@ import {
   ScrollView,
   LayoutAnimation,
   Platform,
-  UIManager
+  UIManager,
 } from "react-native";
 import { Colors } from "../styles/Colors";
-import { Avatar } from "@rneui/themed";
+import { Avatar, Icon } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
 
 //Navigation
 import { useNavigation } from "@react-navigation/native";
 
-
-const ExpandableComponent = ({item, onClickFunction}) => {
-  const [layoutHeight, setlayoutHeight] = useState(0)
+const ExpandableComponent = ({ item, onClickFunction }) => {
+  const [layoutHeight, setlayoutHeight] = useState(0);
 
   useEffect(() => {
-    if(item.isExpanded) {
+    if (item.isExpanded) {
       setlayoutHeight(null);
     } else {
       setlayoutHeight(0);
     }
-  }, [item.isExpanded])
-  
+  }, [item.isExpanded]);
 
-  return(
+  return (
     <View>
-      <TouchableOpacity 
-        style={styles.item}
-        onPress={onClickFunction}
-        >
-        <Text style={styles.itemText}>
-          {item.category_name}
-        </Text>
-
+      <TouchableOpacity style={styles.item} onPress={onClickFunction}>
+        <Text style={styles.itemText}>{item.category_name}</Text>
       </TouchableOpacity>
-      <View style={{height: layoutHeight, overflow: 'hidden'}}>
-      {
-        item.subCategory.map((item, key) => (
-          <View 
-          key={key}
-          style={styles.content}
-          >
+      <View style={{ height: layoutHeight, overflow: "hidden" }}>
+        {item.subCategory.map((item, key) => (
+          <View key={key} style={styles.content}>
             <Text style={styles.text}>
-              Id: {key} {"\n"} 
+              Id: {key} {"\n"}
               Estado: {item.estado} {"\n"}
               Fecha: {item.fecha} {"\n"}
               Hora: {item.hora}hs {"\n"}
-              Comentarios: {item.comentario} 
+              Comentarios: {item.comentario}
             </Text>
           </View>
-        ))
-      }
+        ))}
       </View>
     </View>
-  ) 
-}
-
+  );
+};
 
 const CONTENT = [
-    {
-      isExpanded: false,
-      category_name: 'Viaje 1',
-      subCategory: [
-        {
-          id: 0, 
-          estado: 'Descargando',
-          fecha: '03/07/2023',
-          hora: '10:30',
-          comentario: 'esto es un comentario',
-        },
-      ]
-    },
-    {
-      isExpanded: false,
-      category_name: 'Viaje 2',
-      subCategory: [
-        {
-          id: 1, 
-          estado: 'Cargando',
-          fecha: '15/07/2023',
-          hora: '18:00',
-          comentario: 'esto es un comentario mejor que el anterior',
-        },
-      ]
-    },
-    {
-      isExpanded: false,
-      category_name: 'Viaje 3',
-      subCategory: [
-        {
-          id: 2, 
-          estado: 'Finalizado',
-          fecha: '25/07/2023',
-          hora: '10:30',
-          comentario: 'esto es un comentario, que buen comentario',
-        },
-      ]
-    },
-    {
-      isExpanded: false,
-      category_name: 'Viaje 4',
-      subCategory: [
-        {
-          id: 3, 
-          estado: 'Descargando',
-          fecha: '03/07/2023',
-          hora: '10:30',
-          comentario: 'esto es un comentario',
-        },
-      ]
-    },
-    {
-      isExpanded: false,
-      category_name: 'Viaje 5',
-      subCategory: [
-        {
-          id: 4, 
-          estado: 'Cargando',
-          fecha: '15/07/2023',
-          hora: '18:00',
-          comentario: 'esto es un comentario mejor que el anterior',
-        },
-      ]
-    },
-    {
-      isExpanded: false,
-      category_name: 'Viaje 6',
-      subCategory: [
-        {
-          id: 5, 
-          estado: 'Finalizado',
-          fecha: '25/07/2023',
-          hora: '10:30',
-          comentario: 'esto es un comentario, que buen comentario',
-        },
-      ]
-    },
-  ]
-
-
-
+  {
+    isExpanded: false,
+    category_name: "Viaje 1",
+    subCategory: [
+      {
+        id: 0,
+        estado: "Descargando",
+        fecha: "03/07/2023",
+        hora: "10:30",
+        comentario: "esto es un comentario",
+      },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Viaje 2",
+    subCategory: [
+      {
+        id: 1,
+        estado: "Cargando",
+        fecha: "15/07/2023",
+        hora: "18:00",
+        comentario: "esto es un comentario mejor que el anterior",
+      },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Viaje 3",
+    subCategory: [
+      {
+        id: 2,
+        estado: "Finalizado",
+        fecha: "25/07/2023",
+        hora: "10:30",
+        comentario: "esto es un comentario, que buen comentario",
+      },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Viaje 4",
+    subCategory: [
+      {
+        id: 3,
+        estado: "Descargando",
+        fecha: "03/07/2023",
+        hora: "10:30",
+        comentario: "esto es un comentario",
+      },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Viaje 5",
+    subCategory: [
+      {
+        id: 4,
+        estado: "Cargando",
+        fecha: "15/07/2023",
+        hora: "18:00",
+        comentario: "esto es un comentario mejor que el anterior",
+      },
+    ],
+  },
+  {
+    isExpanded: false,
+    category_name: "Viaje 6",
+    subCategory: [
+      {
+        id: 5,
+        estado: "Finalizado",
+        fecha: "25/07/2023",
+        hora: "10:30",
+        comentario: "esto es un comentario, que buen comentario",
+      },
+    ],
+  },
+];
 
 const ListaDeViajesScreen = () => {
   const nav = useNavigation();
- 
-  const [multiSelect, setmultiSelect] = useState(false)
 
-  const [listDataSource, setlistDataSource] = useState(CONTENT)
+  const [multiSelect, setmultiSelect] = useState(false);
 
-  if(Platform.OS === 'android') {
+  const [listDataSource, setlistDataSource] = useState(CONTENT);
+
+  if (Platform.OS === "android") {
     UIManager.setLayoutAnimationEnabledExperimental(true);
   }
 
   const updateLayout = (index) => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     const array = [...listDataSource];
-    if(multiSelect) {
+    if (multiSelect) {
       //multiple select is enable
-      array[index]['isExpanded'] = !array[index]['isExpanded']
-    }else {
+      array[index]["isExpanded"] = !array[index]["isExpanded"];
+    } else {
       //simple select is enable
       array.map((value, placeindex) =>
-      placeindex === index
-      ? (array[placeindex]['isExpanded']) = !array[placeindex]['isExpanded']
-      : (array[placeindex]['isExpanded']) = false
+        placeindex === index
+          ? (array[placeindex]["isExpanded"] = !array[placeindex]["isExpanded"])
+          : (array[placeindex]["isExpanded"] = false)
       );
     }
-    setlistDataSource(array)
-  }
-
-
+    setlistDataSource(array);
+  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => setmultiSelect(!multiSelect)}>
-        <Text style={{textAlign: 'center', justifyContent:'center'}}>
-          {
-            multiSelect
-            ? 'Multiple'
-            : 'Individual'
-          }
-        </Text>
+      <TouchableOpacity
+        onPress={() => setmultiSelect(!multiSelect)}
+        style={styles.multiSelectButtom}
+      >
+        <Icon
+          size={30}
+          type="material-community"
+          name={multiSelect ? "plus-box-multiple" : "minus-box-multiple"}
+        />
       </TouchableOpacity>
-      <ScrollView style={{width: '100%'}}>
-          {
-            listDataSource.map((item, key) => (
-              <ExpandableComponent
-                key={item.category_name}
-                item={item}
-                onClickFunction={() => {
-                  updateLayout(key)
-                }}
-              />
-            ))
-          }
+      <ScrollView style={{ width: "100%" }}>
+        {listDataSource.map((item, key) => (
+          <ExpandableComponent
+            key={item.category_name}
+            item={item}
+            onClickFunction={() => {
+              updateLayout(key);
+            }}
+          />
+        ))}
       </ScrollView>
-      
-
-
-
-
-
       <View style={styles.addBtn}>
         <TouchableOpacity onPress={() => nav.navigate("CrearViaje")}>
           <Avatar
@@ -218,6 +193,7 @@ const ListaDeViajesScreen = () => {
           />
         </TouchableOpacity>
       </View>
+      <StatusBar style="light" />
     </View>
   );
 };
@@ -229,7 +205,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: Colors.grey,
+    backgroundColor: Colors.lightBlue,
   },
   addBtn: {
     position: "absolute",
@@ -282,11 +258,11 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     padding: 20,
     borderBottomWidth: 0.5,
-    borderBottomColor: '#c8c8c8',
+    borderBottomColor: "#c8c8c8",
   },
   itemText: {
     fontSize: 16,
-    fontFamily: 'nunito'
+    fontFamily: "nunito",
   },
   content: {
     paddingLeft: 10,
@@ -297,9 +273,18 @@ const styles = StyleSheet.create({
     fontSize: 14,
     padding: 10,
   },
+  //MultiselectButtom
+  multiSelectButtom: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 999,
+    padding: 10,
+  },
 });
 
-{//MODAL
+{
+  //MODAL
   /*
   const [modalVisible, setModalVisible] = useState(false);
     
@@ -343,5 +328,3 @@ const styles = StyleSheet.create({
         <MyButton label={"OpenModal"} onPress={() => setModalVisible(true)} />
       </View>*/
 }
-
-//List React-native Elements
