3d2e7a4006e3544316c88d01630469ed26b580b1
3iernes <fede-2800@hotmail.com>
2023-01-12 13:25:05 -0300

    Peticion API local

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const createUser = async(userData)=>{
-    return axios.post('http://192.168.0.169:4005/api/user',userData)
+    return axios.post('http://192.168.0.198:4005/api/user',userData)
 }
