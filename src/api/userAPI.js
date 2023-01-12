7f4397a7b1e3de72eb6587ca593b65d0a2f0526b
3iernes <fede-2800@hotmail.com>
2023-01-12 13:22:52 -0300

    Se quito peticion de prueba

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-export const getUsers = async()=>{
-    return axios.get('https://jsonplaceholder.typicode.com/users')
-}
 export const createUser = async(userData)=>{
     return axios.post('http://192.168.0.169:4005/api/user',userData)
 }
