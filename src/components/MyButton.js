b1bfbce1aa19d4a31631f828f582564d5de795a4
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-17 22:33:48 -0300

    Ajuste de diseño del componente boton

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -2,39 +2,21 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { Colors } from "../styles/Colors";
 
-const SIZES = ["small", "medium", "large"];
-const TYPES = ["primary", "secondary"];
-const ROUNDED = ["small", "medium", "large"];
 
 export default function MyButton({
   children,
   onPress,
-  type,
-  size,
   label,
-  rounded,
 }) {
 
-  
-  const btnSize = SIZES.includes(size) ? size : "small";
-  const btnType = TYPES.includes(type) ? type : "primary";
-  const btnRound = ROUNDED.includes(rounded) ? rounded : "small";
-
   const btnStyles = {
-    height: 60,
-    width: btnSize === "large" ? "70%" : btnSize === "medium" ? 210 : 180,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 16,
-    paddingVertical: 6,
-    borderRadius: btnRound === "small" ? 5 : btnRound === "medium" ? 10 : 20,
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 5,
     marginVertical: 5,
-    backgroundColor:
-      btnType === "primary"
-        ? Colors.primary
-        : btnType === "secondary"
-        ? Colors.secondary
-        : btnType === "",
+    backgroundColor: Colors.primary,
   };
 
   return (
@@ -42,25 +24,9 @@ export default function MyButton({
     style={btnStyles}
     onPress={onPress}
     >
-      <Text style={{ color: "#fff", fontSize: 17, fontWeight: 'bold'}}>
+      <Text style={{ color: "#fff", fontSize: 14, fontWeight: 'bold'}}>
         {label}
       </Text>
     </TouchableOpacity>
   );
 }
-
-
-{/* Ejemplo de como usar componente
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MyButton label={'Messi'} onPress={() => alert('Messi')}/>
-      <MyButton label={'Messi Messi'} size={'medium'} rounded={'medium'}/>
-      <MyButton label={'Fultbol Futbol Futbol'} size={'large'} rounded={'large'} type={'secondary'}/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-*/}
