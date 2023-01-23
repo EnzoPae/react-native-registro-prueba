33edc2b86880dbb86899b6f221ccef0713529d02
3iernes <fede-2800@hotmail.com>
2023-01-23 14:06:19 -0300

    Se cambio el mensaje del typeError

--- src/Schemas/signupFormSchema.js
+++ src/Schemas/signupFormSchema.js
@@ -3,9 +3,9 @@ import * as yup from "yup";
 export const signUpValidationSchema = yup.object().shape({
   name: yup
     .string()
-    .matches(/(\w.+\s).+/, "Enter at least 2 names")
+    .matches(/(\w.+\s).+/, "Ingrese nombre y apellido")
     .required("El nombre completo es obligatorio")
-    .typeError("Pone letras"),
+    .typeError("Ingrese nombre y apellido"),
   phoneNumber: yup
     .string()
     .matches(/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/, "Enter a valid phone number")
@@ -14,15 +14,15 @@ export const signUpValidationSchema = yup.object().shape({
     .string()
     .email("Ingrese un mail valido")
     .required("El email es obligatorio")
-    .typeError("Pone letras"),
+    .typeError("Ingrese un mail valido"),
   password: yup
     .string()
     .min(4, ({ min }) => `La contraseña debe tener al menos ${min} caracteres`)
     .required("La contraseña es obligatoria")
-    .typeError("Pone letras"),
+    .typeError("La contraseña es obligatoria"),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("password")], "Las contraseñas no coinciden")
     .required("Debe confirmar su contraseña")
-    .typeError("Pone letras"),
+    .typeError("Debe confirmar su contraseña"),
 });
