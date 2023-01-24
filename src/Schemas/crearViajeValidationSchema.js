b1444da1fe28fd0d2dba5937285aef4cd7b7c73c
3iernes <fede-2800@hotmail.com>
2023-01-24 01:20:29 -0300

    se quito validacion para origen y destino

--- src/Schemas/crearViajeValidationSchema.js
+++ src/Schemas/crearViajeValidationSchema.js
@@ -1,14 +1,6 @@
 import * as yup from "yup";
 
 export const crearViajeValidationSchema = yup.object().shape({
-  origen: yup
-    .string()
-    .required("La ubicacion de origen es obligatoria")
-    .typeError("Este campo es obligatorio"),
-  destino: yup
-    .string()
-    .required("La ubicacion de destino es obligatoria")
-    .typeError("Este campo es obligatorio"),
   cantidad: yup
     .string()
     .matches(/^[0-9]*$/, "Solo acepta numeros")
