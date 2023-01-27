013957d09cb097b2d4a6a3a0580b50b2c29d403f
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-26 21:53:52 -0300

    Se elimino font loaded

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import React from "react";
 import { useEffect } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
 import MyNavigation from "./src/navigation/navigation";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import { AxiosProvider } from "./src/contexts/AxiosContext";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    nunito: require("./assets/fonts/Nunito-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-    prepare();
-  }, []);
-
-  if (!fontsLoaded) {
-    return undefined;
-  } else {
-    SplashScreen.hideAsync();
-  }
-
+  
   return (
     <AuthProvider>
       <NavigationContainer>
