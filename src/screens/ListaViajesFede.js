60b2e294e92f2f8fa7ee77c6f9b5474fbf5482d3
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-01-31 15:00:38 -0300

    Implementacion de Scroll

--- src/screens/ListaViajesFede.js
+++ src/screens/ListaViajesFede.js
@@ -1,82 +1,94 @@
-import React, { useState, useEffect, useContext, useMemo } from 'react'
-import { useIsFocused } from '@react-navigation/native';
-import { ListItem, Icon } from '@rneui/themed';
-import { AxiosContext } from '../contexts/AxiosContext'
-import ModalAlert from '../components/ModalAlert';
-import Spinner from '../components/Spinner';
-import { SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { useIsFocused } from "@react-navigation/native";
+import { ListItem, Icon } from "@rneui/themed";
+import { AxiosContext } from "../contexts/AxiosContext";
+import ModalAlert from "../components/ModalAlert";
+import Spinner from "../components/Spinner";
+import { SafeAreaView, ScrollView } from "react-native";
 export default function ListaViajesFede() {
-  const isFocused = useIsFocused()
-  const { authAxios } = useContext(AxiosContext)
-  const [expandedItems, setExpanded] = useState([])
-  const [trips, setTrips] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [msj, setMsj] = useState(null)
+  const isFocused = useIsFocused();
+  const { authAxios } = useContext(AxiosContext);
+  const [expandedItems, setExpanded] = useState([]);
+  const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [msj, setMsj] = useState(null);
   useEffect(() => {
     if (isFocused) {
       const getTrips = async () => {
         try {
-          setLoading(true)
-          const response = await authAxios.get('/api/trips')
-          setTrips(response.data)
+          setLoading(true);
+          const response = await authAxios.get("/api/trips");
+          setTrips(response.data);
         } catch (error) {
-          setError(true)
-          setMsj(error.response.data.msj)
+          setError(true);
+          setMsj(error.response.data.msj);
         } finally {
-          setLoading(false)
+          setLoading(false);
         }
-      }
-      getTrips()
+      };
+      getTrips();
     } else {
-      setError(false)
-      setLoading(false)
-      setTrips([])
+      setError(false);
+      setLoading(false);
+      setTrips([]);
     }
-  }, [isFocused])
-  if(loading) return <Spinner/>
+  }, [isFocused]);
+  if (loading) return <Spinner />;
   return (
     <SafeAreaView>
-      {!loading ? trips.map((v, i) => {
-        return (
-          <ListItem.Accordion
-            key={`accordion${i}`}
-            content={
-              <>
-                <Icon name="place" size={30} />
-                <ListItem.Content>
-                  <ListItem.Title>{v.desc_localidad_o} - {v.desc_localidad_d}</ListItem.Title>
-                </ListItem.Content>
-              </>
-            }
-            isExpanded={expandedItems.includes(i)}
-            onPress={() => {
-              if (expandedItems.includes(i)) {
-                setExpanded(expandedItems.filter(id => id !== i));
-              } else {
-                setExpanded([...expandedItems, i]);
-              }
-            }}
-          >
-            {/*CONTENIDO DEL ACORDION*/}
-            <ListItem key={`item${i}`} bottomDivider>
-              <ListItem.Content>
-                <ListItem.Title>*Origen</ListItem.Title>
-                <ListItem.Subtitle>{v.desc_localidad_o}</ListItem.Subtitle>
-                <ListItem.Title>*Destino</ListItem.Title>
-                <ListItem.Subtitle>{v.desc_localidad_d}</ListItem.Subtitle>
-                <ListItem.Title>*Fecha</ListItem.Title>
-                <ListItem.Subtitle>{String(v.fecha_viaje).split('T')[0]} - {String(v.fecha_viaje).split('T')[1].slice(0,8)}</ListItem.Subtitle>
-              </ListItem.Content>
-              <ListItem.Chevron />
-            </ListItem>
-          </ListItem.Accordion>
-        )
-      }) : null}
-      <ModalAlert
-        modalVisible={error}
-        setModalVisible={setError}
-        msj={msj} />
+      <ScrollView
+        style={{ width: "100%" }}
+      >
+        {!loading
+          ? trips.map((v, i) => {
+              return (
+                <ListItem.Accordion
+                  key={`accordion${i}`}
+                  content={
+                    <>
+                      <Icon name="place" size={30} />
+                      <ListItem.Content>
+                        <ListItem.Title>
+                          {v.desc_localidad_o} - {v.desc_localidad_d}
+                        </ListItem.Title>
+                      </ListItem.Content>
+                    </>
+                  }
+                  isExpanded={expandedItems.includes(i)}
+                  onPress={() => {
+                    if (expandedItems.includes(i)) {
+                      setExpanded(expandedItems.filter((id) => id !== i));
+                    } else {
+                      setExpanded([...expandedItems, i]);
+                    }
+                  }}
+                >
+                  {/*CONTENIDO DEL ACORDION*/}
+                  <ListItem key={`item${i}`} bottomDivider>
+                    <ListItem.Content>
+                      <ListItem.Title>*Origen</ListItem.Title>
+                      <ListItem.Subtitle>
+                        {v.desc_localidad_o}
+                      </ListItem.Subtitle>
+                      <ListItem.Title>*Destino</ListItem.Title>
+                      <ListItem.Subtitle>
+                        {v.desc_localidad_d}
+                      </ListItem.Subtitle>
+                      <ListItem.Title>*Fecha</ListItem.Title>
+                      <ListItem.Subtitle>
+                        {String(v.fecha_viaje).split("T")[0]} -{" "}
+                        {String(v.fecha_viaje).split("T")[1].slice(0, 8)}
+                      </ListItem.Subtitle>
+                    </ListItem.Content>
+                    <ListItem.Chevron />
+                  </ListItem>
+                </ListItem.Accordion>
+              );
+            })
+          : null}
+        <ModalAlert modalVisible={error} setModalVisible={setError} msj={msj} />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
