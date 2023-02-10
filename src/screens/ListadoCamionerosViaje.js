0385e5e230b096da9a04d4c7c311a4c814f69cd9
Federico Acosta <fede-2800@hotmail.com>
2023-02-10 13:06:22 -0300

    src/screens/ListadoCamionerosViaje.js

--- src/screens/ListadoCamionerosViaje.js
+++ src/screens/ListadoCamionerosViaje.js
@@ -4,61 +4,18 @@ import {
     ScrollView,
     Text,
 } from "react-native";
-import { useIsFocused } from "@react-navigation/native";
-//Axios
-import { AxiosContext } from "../contexts/AxiosContext";
 //Components
 import { ListItem, Icon } from "@rneui/themed";
-import Spinner from "../components/Spinner";
-import ModalAlert from "../components/ModalAlert";
 //Styles
 import { tripListStyles } from "../styles/GlobalStyles";
 
 export default function ListadoCamionerosViaje({ route }) {
-    const viajeParam = route.params
-    console.log(viajeParam)
-    const isFocused = useIsFocused();
-    const { authAxios } = useContext(AxiosContext);
-    const [trucks, setTrucks] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const drivers = route.params
     const [expandedItems, setExpanded] = useState([]);
-    const [error, setError] = useState(false);
-    const [msj, setMsj] = useState(null);
-    /*
-      Verificar si se esta viendo la pantalla mediante isFocused
-    */
-    const getTrucks = async () => {
-        try {
-            setLoading(true);
-            const response = await authAxios.get("/api/app/vehicles");
-            setTrucks(response.data);
-        } catch (error) {
-            setError(true);
-            setMsj(error.response.data.msj);
-        } finally {
-            setLoading(false);
-        }
-    };
-    useEffect(() => {
-        if (isFocused) {
-            //Con el focus ya en la pantalla
-            getTrucks();
-        } else {
-            /*
-            Si se pierde el foco de la pantalla los
-            estados vuelven a sus valores iniciales
-          */
-            setError(false);
-            setLoading(false);
-            setTrucks([]);
-        }
-    }, [isFocused]);
-    if (loading) return <Spinner />;
     return (
         <SafeAreaView>
             <ScrollView style={{ width: "100%" }}>
-                {!loading
-                    ? trucks.map((v, i) => {
+                {drivers.map((v, i) => {
                         return (
                             <ListItem.Accordion
                                 topDivider
@@ -94,13 +51,7 @@ export default function ListadoCamionerosViaje({ route }) {
                                 </ListItem>
                             </ListItem.Accordion>
                         );
-                    })
-                    : null}
-                <ModalAlert
-                    modalVisible={error}
-                    setModalVisible={setError}
-                    msj={msj}
-                />
+                    })}
             </ScrollView>
         </SafeAreaView>
     );
