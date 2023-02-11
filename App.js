57b4a79af5af761882511b1199f034c0e1c74a88
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-02-11 12:55:27 -0300

    Importacion de StatusBar desde App.js para solucionar problema al momento de carga de aplicacion, ahora siempre carga primero el statusBar

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import MyNavigation from "./src/navigation/navigation";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import { AxiosProvider } from "./src/contexts/AxiosContext";
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Colors } from "./src/styles/Colors";
 export default function App() {
   
   return (
     <AuthProvider>
       <NavigationContainer>
         <AxiosProvider>
+          <>
+          <StatusBar style="light" backgroundColor={Colors.primary}/>
           <MyNavigation />
+          </>
         </AxiosProvider>
       </NavigationContainer>
     </AuthProvider>
