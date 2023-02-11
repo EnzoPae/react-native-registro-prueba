0d7f8507982175cb8d6957a5908ed5a584a2225c
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-02-11 16:09:26 -0300

    Se agrego un nuevo tipo de estilos para el componente MyButton, para el uso en las listas de viajes

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -2,31 +2,32 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { Colors } from "../styles/Colors";
 
+const TYPES = ["normal", "trip-list"];
 
-export default function MyButton({
-  children,
-  onPress,
-  label,
-}) {
+export default function MyButton({ children, onPress, label, type }) {
+  const btnType = TYPES.includes(type) ? type : "normal";
 
   const btnStyles = {
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 30,
-    paddingVertical: 15,
-    borderRadius: 5,
+    paddingHorizontal: btnType === 'trip-list' ? 10 : 30,
+    paddingVertical: btnType === 'trip-list' ? 7 : 15,
+    borderRadius: btnType === 'trip-list' ? 20 : 5,
     marginVertical: 5,
+    marginRight: btnType === 'trip-list' ? 10 : 5,
     backgroundColor: Colors.primary,
+
+  };
+
+  const btnTextStyles = {
+    color: "#fff",
+    fontSize: btnType === 'trip-list' ? 12 : 14,
+    fontWeight: "bold",
   };
 
   return (
-    <TouchableOpacity 
-    style={btnStyles}
-    onPress={onPress}
-    >
-      <Text style={{ color: "#fff", fontSize: 14, fontWeight: 'bold'}}>
-        {label}
-      </Text>
+    <TouchableOpacity style={btnStyles} onPress={onPress}>
+      <Text style={btnTextStyles}>{label}</Text>
     </TouchableOpacity>
   );
 }
