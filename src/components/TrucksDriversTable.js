f783f7a2e3e4c05ebc54bc4cc491630ae9c389ba
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2023-02-11 18:17:21 -0300

    Ajustes de diseño en la tabla de asignar Chofer a un viaje. Saque algunos datos pero lo deje comentado. falta añadir scroll

--- src/components/TrucksDriversTable.js
+++ src/components/TrucksDriversTable.js
@@ -1,89 +1,100 @@
-import { useEffect, useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import {
-    View,
-    Text,
-    SafeAreaView,
-    FlatList,
-    StyleSheet,
-    TouchableOpacity,
-    StatusBar
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
 } from "react-native";
 import Spinner from "./Spinner";
 import { AxiosContext } from "../contexts/AxiosContext";
+import { Colors } from "../styles/Colors";
 const TruckDriverItem = ({ item, onPress }) => (
-    <TouchableOpacity onPress={onPress} style={[styles.item]}>
-        <View style={{ flexDirection: 'row'}}>
-            <Text>{String(item.camion).toUpperCase()}</Text>
+  <TouchableOpacity onPress={onPress} style={[styles.item]}>
+    <View style={{ flexDirection: "row" }}>
+    {
+        /*
+            <Text style={styles.text}>{String(item.camion).toUpperCase()}</Text>
             <Text>{` | `}</Text>
-            <Text>{String(item.batea).toUpperCase()}</Text>
+            <Text style={styles.text}>{String(item.batea).toUpperCase()}</Text>
             <Text>{` | `}</Text>
-            <Text>{String(item.apenom).toUpperCase()}</Text>
-            <Text>{` | `}</Text>
-            <Text>{item.dni}</Text>
-        </View>
-    </TouchableOpacity>
+        */
+    }
+      <Text style={styles.text}>{String(item.apenom).toUpperCase()}</Text>
+      <Text>{` | `}</Text>
+      <Text style={styles.text}>{item.dni}</Text>
+    </View>
+  </TouchableOpacity>
 );
-export default function TrucksDriversTable({handleLinkTruckDriver}) {    
-    const [data,setData] = useState([])
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false);
-    const {authAxios} = useContext(AxiosContext)
+export default function TrucksDriversTable({ handleLinkTruckDriver }) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { authAxios } = useContext(AxiosContext);
 
-    const fetchTrucksDrivers = async () => {
-        try {
-            setLoading(true)
-            const api_response = await authAxios.get('/api/app/vehicles')
-            setData(api_response.data)
-        } catch (error) {
-            setError('Error al recuperar choferes')
-        }finally{
-            setLoading(false)
-        }
+  const fetchTrucksDrivers = async () => {
+    try {
+      setLoading(true);
+      const api_response = await authAxios.get("/api/app/vehicles");
+      setData(api_response.data);
+    } catch (error) {
+      setError("Error al recuperar choferes");
+    } finally {
+      setLoading(false);
     }
-    useEffect(() => {
-        fetchTrucksDrivers()
-    }, [])
+  };
+  useEffect(() => {
+    fetchTrucksDrivers();
+  }, []);
 
-    const handleClik = (item) => {
-        handleLinkTruckDriver(item.id)
-    }
+  const handleClik = (item) => {
+    handleLinkTruckDriver(item.id);
+  };
 
-    const renderItem = ({ item }) => {
-
-        return (
-            <TruckDriverItem
-                item={item}
-                onPress={() => handleClik(item)}
-            />
-        );
-    };
-    if (loading) return <Spinner />;
-    if(error) return <SafeAreaView><Text>{error}</Text></SafeAreaView>
+  const renderItem = ({ item }) => {
+    return <TruckDriverItem item={item} onPress={() => handleClik(item)} />;
+  };
+  if (loading) return <Spinner />;
+  if (error)
     return (
-        <SafeAreaView>
-            <FlatList
-                style={{marginTop: 28}}
-                data={data}
-                renderItem={renderItem}
-                onPress={() => handleClik(data)}
-                keyExtractor={item => item.id}
-            />
-        </SafeAreaView>
-    )
+      <SafeAreaView>
+        <Text>{error}</Text>
+      </SafeAreaView>
+    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Asignar Chofer</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        onPress={() => handleClik(data)}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        padding: 12,
-        marginVertical: 3,
-        backgroundColor: '#dedede',
-        borderRadius: 5
-    },
-    title: {
-        fontSize: 32,
-    },
+  container: {
+    flex: 1,
+    margin: 20,
+    width: '90%'
+  },
+  item: {
+    padding: 5,
+    backgroundColor: Colors.white,
+    marginVertical: 3,
+    borderRadius: 3,
+    borderWidth: 0.5,
+  },
+  text: {
+    fontSize: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
 });
